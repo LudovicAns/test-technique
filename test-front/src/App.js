06e516942c2572a80fab61acf30df121a6165f9b
LudovicAns <ludovicanselin@gmail.com>
2022-08-12 15:22:29 +0200

    I don't know what I'm doing wrong, I can't any keys in movie. I think it's the laste commit so 😥.

--- test-front/src/App.js
+++ test-front/src/App.js
@@ -1,22 +1,21 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import MovieCard from './components/MovieCard';
 
 let API_key = "&api_key=ad2c28e0345278f3c8b002efddadf28f";
 let api_url = "https://api.themoviedb.org/3";
 let url = api_url + "/discover/movie?sort_by=release_date.desc" + API_key;
 
 function App() {
-  useEffect(() => {
+  function loadMovies() {
     fetch(url)
       .then((response) => response.json())
-      .then((data) => console.log(data))
-  });
-  return (
-    <>
-      <MovieCard />
-    </>
-  )
+      .then((data) => setMovie(data.results))
+  }
+
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {loadMovies()});
+
+  return <p>{movie.original_language}</p>
 }
 
 export default App;
